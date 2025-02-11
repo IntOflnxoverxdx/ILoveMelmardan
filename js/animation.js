7ed63ddcf6f8abe7579716a516e8a435b6f6f636
IntOflnxoverxdx <106513380+IntOflnxoverxdx@users.noreply.github.com>
2025-02-12 00:22:14 +1000

    Add files via upload

--- js/animation.js
+++ js/animation.js
@@ -1,14 +1,32 @@
-text = document.querySelector(".heart__letters").innerHTML
+text = document.querySelector(".heart__text").textContent
+text.split("").forEach(letter => {
+    document.querySelector(".heart__letters").innerHTML += `<span class="heart__letter" ${letter == " " ? 'style="color:transparent"' : ""}>${letter == " " ? "c" : letter}</span>`
+});
+
+
+let odd = 1
+document.querySelectorAll(".card").forEach(card => {
+    card.onclick = () => { 
+        if (odd) card.classList.add("slideLeft")
+        if (!odd) card.classList.add("slideRight")
+        odd = !odd
+        setTimeout(() => {
+            card.remove()
+            if (document.querySelectorAll(".card").length === 0){
+                document.querySelector(".cards").remove()
+            }
+        }, 900);
+    }
+});
+
 
 document.querySelector(".heart").onclick = () => {
-    document.querySelector(".heart__letters").innerHTML = ""
-    text.split("").forEach(letter => {
-        document.querySelector(".heart__letters").innerHTML += `<span class="heart__letter" ${letter == " " ? 'style="color:#eb4c4c"' : ""}>${letter == " " ? "c" : letter}</span>`
-    });
+    document.querySelector(".heart__letters").style.opacity = 1
+    document.querySelector(".heart__text").remove()
+    document.querySelector(".heart").onclick = () => { }
     document.querySelectorAll(".heart__letter").forEach(letter => {
         letter.style = `animation-delay: ${Math.random()}s; animation-play-state: running;`
     });
-
     setTimeout(() => {
         document.querySelector(".heart").classList.add("active")
         height = document.querySelector(".heart__top").offsetHeight
@@ -20,21 +38,19 @@ document.querySelector(".heart").onclick = () => {
         setTimeout(() => {
             document.querySelector(".heart").classList.add("disabled")
             setTimeout(() => {
-                if (!document.querySelector(".convert").classList.contains("active")) {
-                    document.querySelector(".convert__triangle").style = "transform: scaleY(-1) translate(-50%, 100%);"
-                    document.querySelector(".convert").classList.add("active")
+                document.querySelector(".convert__triangle").style = "transform: scaleY(-1) translate(-50%, 100%);"
+                document.querySelector(".convert").classList.add("active")
+                setTimeout(() => {
+                    document.querySelector(".heart").remove()
+                    document.querySelector(".convert__triangle").style.transform = "scaleY(-1) translate(-50%, 100%)"
+                    document.querySelector(".convert__triangle").style.zIndex = 2
                     setTimeout(() => {
-                        document.querySelector(".convert__triangle").style.transform = "scaleY(-1) translate(-50%, 100%)"
-                        document.querySelector(".convert__triangle").style.zIndex = 2
+                        document.querySelector(".convert__media").classList.add("active")
                         setTimeout(() => {
-
-                            document.querySelector(".convert__media").classList.add("active")
-                            setTimeout(() => {
-                                document.querySelector(".convert__media").style = "z-index:999;"
-                            }, 300);
+                            document.querySelector(".convert__media").style = "z-index:999;"
                         }, 300);
-                    }, 100);
-                }
+                    }, 300);
+                }, 100);
             }, 600);
         }, 1000);
     }, 1700);
